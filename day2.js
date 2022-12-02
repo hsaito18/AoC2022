7927d25fee126f545dceb55a79363fc4fa1aaa2f
Henrique Saito <hsaito2002@gmail.com>
2022-12-02 19:19:38 +0000

    day 2

---
+++ day2.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const input = fs.readFileSync("./inputs/day2.txt").toString().split("\n");
+
+const HAND_SCORE = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const RESULT_SCORE = {};
+
+// A,X - ROCK
+// B,Y - PAPER
+// C,Z - SCISSORS
+const SCORE = {
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
+};
+
+// X - LOSE
+// Y - DRAW
+// Z - WIN
+const SCORE_2 = {
+  "A X": 3, //
+  "A Y": 4,
+  "A Z": 8,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 2,
+  "C Y": 6,
+  "C Z": 7,
+};
+
+let totalScore = 0;
+let totalScore2 = 0;
+for (let line of input) {
+  if (line) {
+    totalScore += SCORE[line];
+    totalScore2 += SCORE_2[line];
+  }
+}
+
+console.log(`Part 1: ${totalScore}`);
+console.log(`Part 2: ${totalScore2}`);
