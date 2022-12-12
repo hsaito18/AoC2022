902369938ccd68e8661b168d84eba2ee21af9cd3
Henrique Saito <hsaito2002@gmail.com>
2022-12-12 21:22:51 +0000

    day 12, learned about A star algo

--- day12.js
+++ day12.js
@@ -1,11 +1,40 @@
 const fs = require("fs");
 let input = fs.readFileSync("./inputs/day12.txt").toString().split("\n");
 input = input.map((l) => l.trim());
+const PriorityQueue = require("js-priority-queue");
+
 // https://en.wikipedia.org/wiki/A*_search_algorithm
 // https://www.npmjs.com/package/js-priority-queue
+
+let result1;
+let result2;
+class Node {
+  constructor(row, col, height) {
+    this.row = row;
+    this.col = col;
+    this.height = height;
+  }
+  getNeighbours() {
+    let neighbours = [];
+    if (this.row !== 0) {
+      neighbours.push(grid[this.row - 1][this.col]);
+    }
+    if (this.col !== 0) {
+      neighbours.push(grid[this.row][this.col - 1]);
+    }
+    if (this.row !== grid.length - 1) {
+      neighbours.push(grid[this.row + 1][this.col]);
+    }
+    if (this.col !== grid[0].length - 1) {
+      neighbours.push(grid[this.row][this.col + 1]);
+    }
+    return neighbours;
+  }
+}
+
 let grid = [];
-let goalPos = [];
-let startPos = [];
+let goalNode;
+let startNode;
 for (let i = 0; i < input.length; i++) {
   if (input[i].length == 0) continue;
   grid[i] = [];
@@ -14,118 +43,77 @@ for (let i = 0; i < input.length; i++) {
     grid[i][j] = currChar;
     if (currChar == "E") {
       goalPos = [i, j];
-      // grid[i][j] = 122 - 97;
-      grid[i][j] = 1;
+      goalNode = new Node(i, j, 25);
+      grid[i][j] = goalNode;
     } else if (currChar == "S") {
-      startPos = [i, j];
-      grid[i][j] = 0;
+      startNode = new Node(i, j, 0);
+      grid[i][j] = startNode;
     } else {
-      grid[i][j] = currChar.charCodeAt() - 97;
+      grid[i][j] = new Node(i, j, currChar.charCodeAt() - 97);
     }
   }
 }
 
-let numRows = input.length;
-let numCols = input[0].length;
-let savedPaths = [];
-for (let i = 0; i < numRows; i++) {
-  savedPaths[i] = [];
+function heuristic(node) {
+  let dx = Math.abs(node.col - goalNode.col);
+  let dy = Math.abs(node.row - goalNode.row);
+  return dx + dy;
 }
 
-function shortestPathTo(row, col) {
-  paths = [];
-  if (row == startPos[0] && col == startPos[1]) {
-    console.log("reached start.");
-    return 0;
+function AStar(start, goal, h) {
+  function getGScore(key) {
+    if (gScore.get(key) === undefined) return Infinity;
+    return gScore.get(key);
   }
-  if (row !== 0 && grid[row][col] - grid[row - 1][col] <= 1) {
-    if (savedPaths[row - 1][col]) {
-      paths.push(savedPaths[row - 1][col]);
-    } else {
-      let sp = shortestPathTo(row - 1, col);
-      paths.push(sp);
-      savedPaths[row - 1][col] = sp;
-    }
+  function getFScore(key) {
+    if (fScore.get(key) === undefined) return Infinity;
+    return fScore.get(key);
   }
-  if (col !== numCols - 1 && grid[row][col] - grid[row][col + 1] <= 1) {
-    if (savedPaths[row][col + 1]) {
-      paths.push(savedPaths[row][col + 1]);
-    } else {
-      let sp = shortestPathTo(row, col + 1);
-      paths.push(sp);
-      savedPaths[row][col + 1] = sp;
-    }
-  }
-  if (row !== numRows - 1 && grid[row][col] - grid[row + 1][col] <= 1) {
-    if (savedPaths[row + 1][col]) {
-      paths.push(savedPaths[row + 1][col]);
-    } else {
-      let sp = shortestPathTo(row + 1, col);
-      paths.push(sp);
-      savedPaths[row + 1][col] = sp;
-    }
-  }
-  if (col !== 0 && grid[row][col] - grid[row][col - 1] <= 1) {
-    if (savedPaths[row][col + 1]) {
-      paths.push(savedPaths[row][col + 1]);
-    } else {
-      let sp = shortestPathTo(row, col + 1);
-      paths.push(sp);
-      savedPaths[row][col + 1] = sp;
-    }
-  }
-  return Math.min(paths) + 1;
-}
+  let openQueue = new PriorityQueue({
+    comparator: (a, b) => getFScore(a) - getFScore(b),
+  });
+  let openSet = new Set();
+  openQueue.queue(start);
+  openSet.add(start);
+  let cameFrom = new Map();
+  let gScore = new Map();
+  let fScore = new Map();
 
-function shortestPathFrom(row, col) {
-  paths = [];
-  if (row == goalPos[0] && col == goalPos[1]) {
-    console.log("reached end.");
-    return 0;
-  }
-  if (row !== 0 && grid[row][col] - grid[row - 1][col] <= 1) {
-    if (savedPaths[row - 1][col]) {
-      paths.push(savedPaths[row - 1][col]);
-    } else {
-      let sp = shortestPathFrom(row - 1, col);
-      paths.push(sp);
-      savedPaths[row - 1][col] = sp;
-    }
-  }
-  if (col !== numCols - 1 && grid[row][col] - grid[row][col + 1] <= 1) {
-    if (savedPaths[row][col + 1]) {
-      paths.push(savedPaths[row][col + 1]);
-    } else {
-      let sp = shortestPathFrom(row, col + 1);
-      paths.push(sp);
-      savedPaths[row][col + 1] = sp;
-    }
-  }
-  if (row !== numRows - 1 && grid[row][col] - grid[row + 1][col] <= 1) {
-    if (savedPaths[row + 1][col]) {
-      paths.push(savedPaths[row + 1][col]);
-    } else {
-      let sp = shortestPathFrom(row + 1, col);
-      paths.push(sp);
-      savedPaths[row + 1][col] = sp;
-    }
-  }
-  if (col !== 0 && grid[row][col] - grid[row][col - 1] <= 1) {
-    if (savedPaths[row][col + 1]) {
-      paths.push(savedPaths[row][col + 1]);
-    } else {
-      let sp = shortestPathFrom(row, col + 1);
-      paths.push(sp);
-      savedPaths[row][col + 1] = sp;
-    }
-  }
-  console.log("actually returning lol");
-  return Math.min(paths) + 1;
-}
-console.log(goalPos);
+  gScore.set(start, 0);
+  fScore.set(start, h(start));
 
-try {
-  console.log(shortestPathFrom(startPos[0], startPos[1]));
-} catch (e) {
-  console.log(savedPaths);
+  while (openQueue.length != 0) {
+    let currentNode = openQueue.dequeue();
+    openSet.delete(currentNode);
+    if (currentNode == goal) {
+      return [getGScore(currentNode), gScore];
+    }
+    let neighbours = currentNode.getNeighbours();
+    for (let neighbour of neighbours) {
+      if (neighbour.height > currentNode.height + 1) continue;
+      let tentativeGScore = getGScore(currentNode) + 1;
+      if (tentativeGScore < getGScore(neighbour)) {
+        gScore.set(neighbour, tentativeGScore);
+        fScore.set(neighbour, tentativeGScore + h(neighbour));
+        if (!openSet.has(neighbour)) {
+          openSet.add(neighbour);
+          openQueue.queue(neighbour);
+        }
+      }
+    }
+  }
+  return [null, null];
 }
+[result1, gScore] = AStar(startNode, goalNode, heuristic);
+let nodes = Array.from(gScore.keys());
+let possibleStarts = nodes.filter((a) => a.height == 0);
+let possibleSteps = [];
+for (let node of possibleStarts) {
+  let steps = AStar(node, goalNode, heuristic);
+  if (steps[0]) {
+    possibleSteps.push(steps[0]);
+  }
+}
+result2 = Math.min(...possibleSteps);
+console.log(`Part 1: ${result1}`);
+console.log(`Part 2: ${result2}`);
