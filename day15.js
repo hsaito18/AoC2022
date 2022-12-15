2fb6bd1e5143229019f8be7aba7c0b966de5cf92
Henrique Saito <hsaito2002@gmail.com>
2022-12-15 21:42:31 +0000

    dear lord day 15

--- day15.js
+++ day15.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 let input = fs.readFileSync("./inputs/day15.txt").toString().split("\n");
 input = input.map((l) => l.trim());
 
-let grid = [];
+const Y_TO_CHECK = 2000000;
+const MAX_COORDS_DISTRESS = 4000000;
+
 let overallXMin = Infinity;
 let overallXMax = -Infinity;
 let sensors = [];
-const yToCheck = 10;
+let beaconsOnY = [];
 
 for (let line of input) {
   if (line.length == 0) continue;
@@ -15,34 +17,88 @@ for (let line of input) {
   let ySensor = Number(words[3].substring(2, words[3].length - 1));
   let xBeacon = Number(words[8].substring(2, words[8].length - 1));
   let yBeacon = Number(words[9].substring(2));
+  if (yBeacon == Y_TO_CHECK) beaconsOnY.push(xBeacon);
   let dist = Math.abs(xBeacon - xSensor) + Math.abs(yBeacon - ySensor);
   sensors.push({ x: xSensor, y: ySensor, dist: dist });
   let xMin = xSensor - dist;
   let xMax = xSensor + dist;
-  let yMin = ySensor - dist;
-  let yMax = ySensor + dist;
   if (xMax > overallXMax) overallXMax = xMax;
   if (xMin < overallXMin) overallXMin = xMin;
-  // for (let x = xMin; x <= xMax; x++) {
-  //   if (!grid[x]) grid[x] = [];
-  //   let yRangeDeduction = Math.abs(x - xSensor);
-  //   for (let y = yMin + yRangeDeduction; y <= yMax - yRangeDeduction; y++) {
-  //     grid[x][y] = true;
-  //   }
-  // }
 }
 
 let spots = 0;
 for (let x = overallXMin; x <= overallXMax; x++) {
+  if (beaconsOnY.includes(x)) continue;
   let spotted = false;
   for (let sensor of sensors) {
-    let currDist = Math.abs(sensor.x - x) + Math.abs(sensor.y - yToCheck);
+    let currDist = Math.abs(sensor.x - x) + Math.abs(sensor.y - Y_TO_CHECK);
     if (currDist <= sensor.dist) {
-      console.log(x);
       spotted = true;
       break;
     }
   }
   if (spotted) spots++;
 }
-console.log(spots);
+
+let result = spots;
+
+let i = 0;
+let edgePoints = [];
+for (let sensor of sensors) {
+  i++;
+  // find all the edge points
+  let y = sensor.y;
+  for (let x = sensor.x + sensor.dist + 1; x >= sensor.x; x--) {
+    edgePoints.push({ x: x, y: y });
+    y++;
+  }
+  y = sensor.y + sensor.dist - 1;
+  for (let x = sensor.x - 1; x >= sensor.x - sensor.dist; x--) {
+    edgePoints.push({ x: x, y: y });
+    y--;
+  }
+  y = sensor.y - 1;
+  for (let x = sensor.x - sensor.dist + 1; x <= sensor.x; x++) {
+    edgePoints.push({ x: x, y: y });
+    y--;
+  }
+  y = sensor.y - sensor.dist + 1;
+  for (let x = sensor.x + 1; x < sensor.x + sensor.dist; x++) {
+    edgePoints.push({ x: x, y: y });
+    y++;
+  }
+}
+
+let trimmedEdgePoints = [];
+for (let i = 0; i < edgePoints.length; i++) {
+  let x = edgePoints[i].x;
+  let y = edgePoints[i].y;
+  if (x > MAX_COORDS_DISTRESS || x < 0 || y > MAX_COORDS_DISTRESS || y < 0) {
+    //
+  } else {
+    trimmedEdgePoints.push({ x: x, y: y });
+  }
+}
+let foundx;
+let foundy;
+for (let i = 0; i < trimmedEdgePoints.length; i++) {
+  let found = true;
+  for (let sensor of sensors) {
+    let currDist =
+      Math.abs(sensor.x - trimmedEdgePoints[i].x) +
+      Math.abs(sensor.y - trimmedEdgePoints[i].y);
+    if (currDist <= sensor.dist) {
+      found = false;
+      break;
+    }
+  }
+  if (found) {
+    foundx = trimmedEdgePoints[i].x;
+    foundy = trimmedEdgePoints[i].y;
+    break;
+  }
+}
+let result2 = foundx * MAX_COORDS_DISTRESS + foundy;
+
+console.log(`Part 1: ${result}`);
+console.log(`Part 2: ${result2}`);
